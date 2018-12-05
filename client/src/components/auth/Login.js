f8cafe40422a7519ab8134dc04b747354fbb2437
aleks <alekszemlyanskiy@gmail.com>
2018-12-05 10:45:49 -0800

    working on login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,13 +1,61 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(user);
+  }
+
   render() {
     return (
       <div className="ui container">
         <br />
         <br />
         <br />
-
+        <form className="ui form" onSubmit={this.onSubmit}>
+          <div className="field">
+            <label>email</label>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.onChange}
+              placeholder="xyz@numerals.com"
+            />
+          </div>
+          <div className="field">
+            <label>password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="******** (testpass)"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+          </div>
+          <button className="ui button" type="submit">
+            Log In
+          </button>
+        </form>
         {/* <form className="ui form" onSubmit={this.onSubmit}>
           <div className="field">
             <label>Nickname</label>
