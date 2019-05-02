cc304eed3ccdda0089dd1b38a860f0ca150b3e9f
austinminich <austin.minich@gmail.com>
2019-05-01 19:34:25 -0700

    Alerts posting function done.

--- routes/api/alerts.js
+++ routes/api/alerts.js
@@ -12,37 +12,42 @@ router.get("/index", (req, res) => res.json({ message: "Hope you see this" }));
 
 router.post(
   "/",
-  passport.authenticate("jwt", { session: false }, (req, res) => {
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     const errors = {};
-    console.log(req.body);
-    console.log(res);
-    alertToInsert = {
-      endpoint: req.body.endpoint, //have to find endpoint
-      model: req.body.model, //chosen model
-      label: req.body.label, //chosen label
-      threshold: req.body.threshold, //chosen threshold
-      timespan: req.body.timespan //TODO: filler MUST CHANGE to now + timespan
-    };
-
     //Does the endpoint and model exist
-    var endpoint = Endpoint.findOne({ name: alertToInsert.endpoint });
-    var model = Model.findOne({ name: alertToInsert.name });
-    if (!endpoint) {
-      errors.endpoint = "Chosen endpoint doesn't exist in database";
-      return res.status(409).json(errors);
-    } else if (!model) {
-      errors.model = "Chosen model doesn't exist in database";
-      return res.status(409).json(errors);
-    }
-    //Check for proper requirements
-    if (req.body.label != "" && req.body.threshold >= 1) {
-      new Alert(alertToInsert) //store
-        .save()
-        .catch(err => {
-          console.log(`ERROR IN MODEL CREATION POST api/models: ${err}`);
-        });
-    }
-  })
+    Endpoint.findOne({ name: req.body.endpoint })
+      .then(foundEndpoint => {
+        Model.findOne({ name: req.body.name })
+          .then(foundModel => {
+            //Check for proper requirements
+            if (req.body.label != "" && req.body.threshold >= 1) {
+              alertToInsert = {
+                endpoint: foundEndpoint, //have to find endpoint
+                model: foundModel, //chosen model
+                label: req.body.label, //chosen label
+                threshold: req.body.threshold, //chosen threshold
+                timespan: req.body.timespan //TODO: filler MUST CHANGE to now + timespan
+              };
+              new Alert(alertToInsert) //store
+                .save()
+                .catch(err => {
+                  console.log(
+                    `ERROR IN ALERT CREATION POST api/alerts: ${err}`
+                  );
+                });
+            }
+          })
+          .catch(error => {
+            errors.model = "Chosen model doesn't exist in database";
+            return res.status(409).json(errors);
+          });
+      })
+      .catch(error => {
+        errors.endpoint = "Chosen endpoint doesn't exist in database";
+        return res.status(409).json(errors);
+      });
+  }
 );
 
 module.exports = router;
