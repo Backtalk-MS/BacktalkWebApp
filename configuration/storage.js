1218b4bbb2120614bec11d354a02ff6aeb340b38
Aleks <alekszemlyanskiy@gmail.com>
2019-05-01 17:13:30 -0700

    uploading working

--- configuration/storage.js
+++ configuration/storage.js
@@ -5,5 +5,11 @@ const path = require("path");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./clientUploads");
+  },
+  filename: (req, file, cb) => {
+    const filename = `${uuidv4()}${path.extname(file.originalname)}`;
+    cb(null, filename);
   }
 });
+
+module.exports = upload = multer({ storage });
