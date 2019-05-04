48b29e13b477af4e08b8485f5f9fe22a59a5df5b
Robert Simons <simonsrobertm@gmail.com>
2019-05-03 18:26:56 -0700

    modified Tab member variable "root"

--- client/src/components/auth/Account.js
+++ client/src/components/auth/Account.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ReactDOM from 'react-dom';
-//import {Tabs, Tablist} from 'Tabs';
-import Tabs from './Tabs';
+//import Tabs from './Tabs';
 
 
 class Account extends Component {
@@ -19,41 +18,15 @@ class Account extends Component {
         const errors = this.state.errors;
         return (
             <div>
-                const Root = () => (
+                
                 <div className="ui container">
-
-                <Tabs.Tabs selected={ 1 }>
-                    <Tabs.TabList>
-                        <Tabs.Tab>
-                            <Tabs.Button>Foo</Tabs.Button>
-                        </Tabs.Tab>
-                        <Tabs.Tab>
-                            <Tabs.Button>Bar</Tabs.Button>
-                        </Tabs.Tab>
-                        <Tabs.Tab>
-                            <Tabs.Button>Baz</Tabs.Button>
-                        </Tabs.Tab>
-                    </Tabs.TabList>
-
-                    <Tabs.TabPanel>
-                        Related to foo
-                    </Tabs.TabPanel>
-
-                    <Tabs.TabPanel>
-                        Related to bar
-                    </Tabs.TabPanel>
-
-                    <Tabs.TabPanel>
-                        Related to baz
-                    </Tabs.TabPanel>
-                </Tabs.Tabs>
-
+                
                     <p>
                         This is the account page. HELLOOOOOO!?
                     </p>
                     
                 </div>
-                )
+                
             </div>
         );
     }  
